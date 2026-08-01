31d98e5219c0b6cab74d94b0f6b99abeade2bcbe
Rajput654 <satyavansingh654@gmail.com>
2026-08-01 19:08:37 +0530

    runnable

---
+++ Runnable/Runnable_passthrough.py
@@ -0,0 +1,34 @@
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableBranch, RunnableLambda
+import os
+load_dotenv()
+
+llm=HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACE_API_TOKEN")
+)
+model=ChatHuggingFace(llm=llm)
+parser=StrOutputParser()
+
+prompt1=PromptTemplate(
+    template='write a joke about {topic}',
+    input_variables=['topic']
+)
+
+prompt2=PromptTemplate(
+    template='explain the following joke -{text}',
+    input_variables=['text']
+)
+
+joke_gen_chain=prompt1|model|parser
+
+parallel_chain=RunnableParallel({
+    'joke':RunnablePassthrough(),
+    'explanation':prompt2|model|parser
+})
+final_chain=RunnableSequence(joke_gen_chain,parallel_chain)
+print(final_chain.invoke({'topic':'AI'}))
